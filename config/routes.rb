03372f217447e025dd41924f4798b33e3c2300a9
Aaron Massey <akmassey@ncsu.edu>
2011-10-14 23:38:30 -0400

    Created basic demonstration of devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Quotes::Application.routes.draw do
   root :to => "home#index"
-
   devise_for :users
+  resources :users, :only => :show
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
