218427b446ea5ff005b7b28536f9ae601e504c6b
Aaron Massey <akmassey@ncsu.edu>
2012-04-13 16:18:09 -0400

    Users should be able to view their own show page.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe UsersController do
+  login_user
 
   describe "GET 'show'" do
     it "should be successful" do
-      get 'show'
+      get :show, :id => subject.current_user.id
       response.should be_success
     end
   end
-
 end
